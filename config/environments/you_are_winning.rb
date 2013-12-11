6ab8e30c6dbd8e03e67097456cb55921d1a47781
DBC Challenges <challenges@devbootcamp.com>
2013-12-11 09:58:29 -0800

    Update you_are_winning.rb

--- config/environments/you_are_winning.rb
+++ config/environments/you_are_winning.rb
@@ -2,6 +2,6 @@
 puts ""
 puts ""
 puts "your secret instructions are:"
-puts "put all your previous commands into a gist and submit it!"
+puts "put all your previous commands into a new gist and submit it!"
 puts ""
 puts "hint: how can you see all your previous commands easily?"
